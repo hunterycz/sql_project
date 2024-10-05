1d81795bd7537297bea55ab8b4f58f38eb6582d8
Hunter Peterson <69177906+hunterycz@users.noreply.github.com>
2024-10-05 11:46:44 -0700

    added queries to the query module

--- queries/queries.py
+++ queries/queries.py
@@ -2,3 +2,33 @@ SELECT_ALL = '''
 SELECT *
 FROM charactercreator_character
 '''
+
+TOTAL_CHARACTERS = '''
+SELECT COUNT(character_id)
+FROM charactercreator_character
+'''
+
+TOTAL_SUBCLASS = '''
+SELECT
+	(SELECT COUNT(*)
+	FROM charactercreator_mage mage
+	INNER JOIN charactercreator_character ccc
+	ON mage.character_ptr_id = ccc.character_id) AS Mage_Count,
+	(SELECT COUNT(*)
+	  FROM  charactercreator_cleric cleric
+	  INNER JOIN charactercreator_character ccc
+	  ON cleric.character_ptr_id = ccc.character_id) AS Cleric_Count,
+	 (SELECT COUNT(*)
+	   FROM charactercreator_thief thief
+	   INNER JOIN charactercreator_character ccc
+	   ON thief.character_ptr_id = ccc.character_id) AS Thief_Count,
+	(SELECT COUNT(*)
+	  FROM charactercreator_fighter fighter
+	  INNER JOIN charactercreator_character ccc
+	  ON fighter.character_ptr_id = ccc.character_id) AS Fighter_Count;
+'''
+
+TOTAL_ITEMS = '''
+SELECT COUNT(*) AS total_items
+FROM armory_item
+'''
